50c897e02b1361b395faed5a1f8e9d408d4da0a4
fajrinthuja <fajrinthuja20@gmail.com>
2026-07-09 23:01:36 +0600

    online a solved

---
+++ matplotlib/22 Online A/template.py
@@ -0,0 +1,107 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ----------------------------
+# Time axis
+# ----------------------------
+T_MIN, T_MAX, N = -4.0, 4.0, 4001
+
+
+def x_of_t(t: np.ndarray) -> np.ndarray:
+    """
+    Base signal x(t): sinusoidal signal
+    """
+    return (
+        np.sin(2 * np.pi * 0.5 * t)
+        + 0.5 * np.sin(2 * np.pi * 1.5 * t)
+    )
+
+
+# ==========================================================
+# ANSWER IMPLEMENTATION
+# ==========================================================
+
+def interpolate_signal(
+    t_original: np.ndarray,
+    x_original: np.ndarray,
+    t_query: np.ndarray
+) -> np.ndarray:
+    """
+    Interpolate using average of two neighboring samples.
+    """
+    y = [0] * len(t_original)
+    t_min = t_original[0]
+    t_max = t_original[-1]
+    dt = t_original[1] - t_min
+    for i in range(len(y)):
+        now = t_query[i]
+        if now <= t_min:
+            y[i] = x_original[0]
+            continue
+        if now >= t_max:
+            y[i] = x_original[-1]
+            continue
+        low = (now - t_min) // dt
+        low = int(low)
+        high = low + 1
+
+        y[i] = (x_original[low] + x_original[high]) / 2
+
+    return y
+
+        
+    # return np.interp(t_query,t_original,x_original) # this one also works but ei jinish ajke shikhlam
+    # raise NotImplementedError
+
+
+def time_scale(
+    t: np.ndarray,
+    x: np.ndarray,
+    k: int
+) -> np.ndarray:
+    """
+    Time sub-scaling:
+        y(t) = x(t / k)
+    """
+    t_scale = t / k
+    return interpolate_signal(t,x,t_scale)
+    # raise NotImplementedError
+
+
+def plot_pair(t: np.ndarray, x: np.ndarray, y: np.ndarray, title: str):
+    """
+    Plot graphs.
+    """
+    fig, axes = plt.subplots(2,1)
+    axes[0].plot(t,y,color = 'r', label = 'y = x(t / k)')
+    axes[1].plot(t,x,color = 'b', label = 'x(t)')
+    axes[0].legend()
+    axes[1].legend()
+    plt.tight_layout()
+    plt.title(str)
+    # fig.title(str)
+    plt.legend()
+    # raise NotImplementedError
+
+
+# ----------------------------
+# Main
+# ----------------------------
+def main():
+    t = np.linspace(T_MIN, T_MAX, N)
+    x = x_of_t(t)
+
+    k = 0.5   # sub-scaling factor
+    y = time_scale(t, x, k)
+
+    plot_pair(
+        t,
+        x,
+        y,
+        title=f"Time Sub-scaling: y(t) = x(t / {k})"
+    )
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
